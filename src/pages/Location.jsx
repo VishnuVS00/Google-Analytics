9c6df6d69f568f7d673d7c863c713d133937157f
Vishnu V S <vishnuvs@techversantinfotech.com>
2024-08-18 18:38:35 +0530

    react ga location

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect } from 'react'
-import ReactGA from "react-ga4";
+import ReactGA4 from "react-ga4";
+
+const TRACKING_ID = "G-2YGDZ9NQ05"
 
 function Location() {
   useEffect(() => {
-    ReactGA.pageview(window.location.pathname)
+    ReactGA4.initialize(TRACKING_ID)
+    ReactGA4.send({ hitType: 'pageview', page: window.location.pathname })
+    // ReactGA4.pageview(window.location.pathname)
   }, [])
 
   return (
     <div className='flex flex-col border border-white h-[70vh] w-full rounded-md p-5'
-     >
+    >
       <h2 className='font-bold'>Location</h2>
       <div className='flex justify-between p-2'>
         <div className='flex gap-3'>
